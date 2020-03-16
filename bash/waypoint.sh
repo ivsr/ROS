d35f803a1ef2fa0c6bc0f31c5202b7298f5ce91b
ctranv <congtranv@outlook.com>
2020-03-16 10:02:19 +0700

    waypoint bash

---
+++ bash/waypoint.sh
@@ -0,0 +1,29 @@
+rosrun rotors_gazebo waypoint_publisher 0 0 1 0 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 1 90 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 1 180 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 1 270 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 1 0 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 2 0 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 2 -90 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 2 -180 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 2 -270 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 2 0 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 3 0 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 3 90 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 3 180 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 3 270 0 __ns:=ardrone
+sleep 5
+rosrun rotors_gazebo waypoint_publisher 0 0 3 0 0 __ns:=ardrone
